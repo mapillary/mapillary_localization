43e329a1c21170f9a735ff67fe090b6c85a923f3
Johan Gyllenspetz <johan.gyllenspetz@gmail.com>
2014-11-13 21:25:55 +0100

    en: camelCase

--- mapillary_localization.js
+++ mapillary_localization.js
@@ -3,13 +3,13 @@
   angular.module("mapapp").config(function($translateProvider) {
     $translateProvider.translations("en", {
       footer: {
-        get_app: "Get the Mapillary App",
+        getApp: "Get the Mapillary App",
         on: "on",
-        direct_android: "Direct Android .apk link",
+        directAndroid: "Direct Android .apk link",
         manifesto: "Manifesto",
         about: "About",
         blog: "Blog",
-        how_it_works: "How It Works",
+        howItWorks: "How It Works",
         capture: "Capture Instructions",
         using: "Using the App",
         positions: "Open Positions",
@@ -22,7 +22,7 @@
         developers: "Developers",
         integrations: "Integrations",
         embed: "Embed",
-        latest_uploads: "Latest Uploads",
+        latestUploads: "Latest Uploads",
         report: "Report Issues",
         follow: "Follow"
       },
@@ -31,7 +31,7 @@
         photos: "photos",
         meters: "meters"
       },
-      landing_business: {
+      landingBusiness: {
         title: "Mapillary for Business",
         use: "Use Mapillary photos",
         service: "Sign up for a commercial API plan in order to use Mapillary photos and data in your service.",
@@ -41,41 +41,41 @@
         gis: "Use our APIs or widget to integrate a complete Mapillary viewer in your application or in your GIS system.",
         learn: "Learn more about our business plans."
       },
-      landing_how: {
+      landingHow: {
         one: "1. Get the Mapillary app",
-        one_availble: "Available for",
+        oneAvailble: "Available for",
         one_from: "and from",
         two: "2. Go out and capture sequences",
         three: "3. Browse, explore, and share places"
       },
-      landing_whatyoucan: {
+      landingWhatyoucan: {
         title: "What you can do with Mapillary",
-        section1_map: "Map a place, a town, or someplace new",
-        section1_missing: "If your town is missing street view photos, you can add it yourself!",
-        section1_examples: "See some great examples at",
-        section1_and: "and",
-        section2_add: "Add roads not on the map, like",
-        section2_road_malaysia: "this road in Malaysia.",
-        section2_future: "Record a place for the future to show what it looked like at a point in time, like this",
-        section2_coastal: "coastal town.",
-        section2_no_cars: "Map an area that cannot be reached with street view cars, like the lovely",
+        section1Map: "Map a place, a town, or someplace new",
+        section1Missing: "If your town is missing street view photos, you can add it yourself!",
+        section1Examples: "See some great examples at",
+        section1And: "and",
+        section2Add: "Add roads not on the map, like",
+        section2RoadMalaysia: "this road in Malaysia.",
+        section2Future: "Record a place for the future to show what it looked like at a point in time, like this",
+        section2Coastal: "coastal town.",
+        section2NoCars: "Map an area that cannot be reached with street view cars, like the lovely",
         section2_venice: "Venice Beach canals,",
-        section2_unmapped: "or the unmapped",
-        section2_fishing: "Venice Fishing Pier.",
-        section2_share: "Share and show",
-        section2_anyone: "Share a place on Mapillary to anyone. Send them a link and it will look good on both mobile and web.",
-        section2_embed: "You can embed a Mapillary viewer on any website with",
-        section2_widget: "our widget.",
-        section2_path: "With this you can show a specific path or route or leave if open for viewers to explore.",
-        section2_see: "See what you can do on",
-        section2_example: "example page.",
-        section3_track: "Track a place over time",
-        section3_filter: "Using the filter functions on Mapillary, you can track a place over time.",
-        section3_older: "Compare recent pictures to older. Show only summer, only winter, and much more.",
-        section3_places: "If you have places and photos that you do don't want to share publicly, you can have private projects. Take a look at our",
-        section3_business: "business plans."
+        section2Unmapped: "or the unmapped",
+        section2Fishing: "Venice Fishing Pier.",
+        section2Share: "Share and show",
+        section2Anyone: "Share a place on Mapillary to anyone. Send them a link and it will look good on both mobile and web.",
+        section2Embed: "You can embed a Mapillary viewer on any website with",
+        section2Widget: "our widget.",
+        section2Path: "With this you can show a specific path or route or leave if open for viewers to explore.",
+        section2See: "See what you can do on",
+        section2Example: "example page.",
+        section3Track: "Track a place over time",
+        section3Filter: "Using the filter functions on Mapillary, you can track a place over time.",
+        section3Older: "Compare recent pictures to older. Show only summer, only winter, and much more.",
+        section3Places: "If you have places and photos that you do don't want to share publicly, you can have private projects. Take a look at our",
+        section3Business: "business plans."
       },
-      landing_search: {
+      landingSearch: {
         header: "Crowdsourced Street Level Photos.",
         search: "Search",
         or: "or",
@@ -83,19 +83,19 @@
         contribute: "contribute your own"
       },
       navbar: {
-        how_it_works: "How It Works",
+        howItWorks: "How It Works",
         explore: "Explore",
         business: "Business",
         developers: "Developers",
         projects: "Projects",
         me: "Me",
-        my_profile: "My Profile",
-        manual_uploads: "Manual Uploads",
+        myProfile: "My Profile",
+        manualUploads: "Manual Uploads",
         settings: "Settings",
-        sign_out: "Sign Out",
+        signOut: "Sign Out",
         or: "or",
-        log_in: "Log In",
-        sign_up: "Sign Up"
+        logIn: "Log In",
+        signUp: "Sign Up"
       },
       explore: {
         title: "Explore",
@@ -136,7 +136,25 @@
         clearAllImages: "Clear all images",
         imageNotUploaded: "Image not uploaded",
         imageUploaded: "Image uploaded",
-        clickToSeeInfo: "Click on image on map to see information about image"
+        clickToSeeInfo: "Click on image on map to see information about image",
+        selectedFile: "Selected file",
+        latitude: "Latitude:",
+        longitude: "Longitude:",
+        angle: "Angle",
+        uploaded: "Uploaded",
+        remove: "Remove",
+        filesWithError: "Files with errors (These files will not be uploaded)",
+        filename: "Filename",
+        step1: "1. Upload the images to the server. Make sure all your files are green and uploaded.",
+        step2: "2. Push your created sequence to Mapillary.",
+        selectProject: "Select project (if leave blank in none)",
+        pushToMapillary: "Push To Mapillary",
+        thankYou: "Thank You!",
+        filesUploaded: "Your files have been uploaded to Mapillary",
+        check: "Check",
+        myUploads: "My Uploads",
+        addedSoon: "they will soon be added to the map",
+        uploadMore: "Upload More"
       }
     });
     $translateProvider.translations("de", {
